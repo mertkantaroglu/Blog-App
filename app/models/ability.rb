56702b4f1d33b93006d8e7e01a798db8c8767ee0
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-06-01 15:49:12 +0300

    Add ability model and role to users

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    can :read, :all
+
+    if user.present?
+      can :manage, Post, author_id: user.id
+
+      if user.admin?
+        can :manage, :all
+      end
+    end
+  end
+end
